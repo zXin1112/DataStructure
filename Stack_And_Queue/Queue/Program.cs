d596e024f940d39e2b78661c4653665012631006
Xin Zhang <zxin1112@gmail.com>
2018-12-04 12:42:36 +0800

    add .md

--- Stack_And_Queue/Queue/Program.cs
+++ Stack_And_Queue/Queue/Program.cs
@@ -3,13 +3,189 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//队列
 namespace Queue
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("创建一个数组实现的队列：");
+            MyArrayQueu<int> myArray = new MyArrayQueu<int>(2);
+            Console.WriteLine("添加数据0-4：");
+            for (int i = 0; i < 5; i++)
+                myArray.EnQueu(i);
+            Console.WriteLine("出队四次：");
+            for (int i = 0; i < 4; i++)
+                Console.Write("{0} ", myArray.DeQueu());
+            Console.WriteLine("\r\n当前队列大小：{0}", myArray.Size);
+            Console.WriteLine("将数据10入队：");
+            myArray.EnQueu(10);
+            Console.WriteLine("将所有队中数据出队：");
+            for (int i = 0, j = myArray.Size; i < j; i++)
+                Console.Write("{0} ", myArray.DeQueu());
+            Console.WriteLine("\r\n---------------------------");
+            Console.WriteLine("创建一个链表实现的队列：");
+            MyLinkQueu<int> myLinkQueu = new MyLinkQueu<int>();
+            Console.WriteLine("添加数据0-4：");
+            for (int i = 0; i < 5; i++)
+                myLinkQueu.EnQueue(i);
+            Console.WriteLine("所有数据出队：");
+            for (int i = 0, j = myLinkQueu.Size; i < j; i++)
+                Console.Write("{0} ", myLinkQueu.DeQueue());
+            Console.WriteLine("\r\n当前队列大小：{0}", myArray.Size);
+            Console.ReadKey();
+        }
+    }
+    //数组实现
+    class MyArrayQueu<T>
+    {
+        private T[] item;//单个数据元素
+        private int size;//队列大小
+        private int head;//队头
+        private int tail;//队尾
+
+        public MyArrayQueu(int capacticy)
+        {
+            item = new T[capacticy];
+            size = 0;
+            head = tail = 0;
+        }
+        /// <summary>
+        /// 入队
+        /// </summary>
+        /// <param name="value"></param>
+        public void EnQueu(T value)
+        {
+            if (size == item.Length) ResizeCapacity(item.Length * 2); //当数组满时，改变大小
+            item[tail] = value;//队尾添加值
+            tail++;//指向下一个
+            size++;
+        }
+        /// <summary>
+        /// 出队
+        /// </summary>
+        /// <returns></returns>
+        public T DeQueu()
+        {
+            if (size == 0) return default(T);
+            T retuValue = item[head];//队头
+            item[head] = default(T);
+            head++;
+            if (head > 0 && size == item.Length / 4)
+                ResizeCapacity(item.Length / 2);
+
+            size--;
+
+            return retuValue;
+        }
+        private void ResizeCapacity(int newCapacity)
+        {
+            T[] newItems = new T[newCapacity];
+            int index = 0;
+            if (newCapacity > item.Length)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    newItems[index++] = item[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (!item[i].Equals(default(T)))//当值不为空时
+                    {
+                        newItems[index++] = item[i];
+                    }
+                }
+                //重置队头队尾
+                head = 0;
+                tail = index;
+            }
+
+            item = newItems;
+
+        }
+        /// <summary>
+        /// 栈是否为空
+        /// </summary>
+        /// <returns>true/false</returns>
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+
+        /// <summary>
+        /// 栈中节点个数
+        /// </summary>
+        public int Size
+        {
+            get
+            {
+                return this.size;
+            }
+        }
+    }
+    //队列节点
+    class Node<T>
+    {
+        public T Item { get; set; }
+        public Node<T> Next { get; set; }
+        public Node() { }
+        public Node(T item)
+        {
+            Item = item;
+        }
+    }
+    //队列链表实现
+    class MyLinkQueu<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private int size;
+        public MyLinkQueu()
+        {
+            size = 0;
+            head = tail = null;
+        }
+        public void EnQueue(T value)
+        {
+            if (size == 0)
+                head = tail = new Node<T>(value);
+            else
+            {
+                tail.Next = new Node<T>(value);
+                tail = tail.Next;
+            }
+            size++;
+        }
+        public T DeQueue()
+        {
+            T retuvalue = head.Item;
+            head = head.Next;
+            size--;
+            if (size == 0) tail = null;
+            return retuvalue;
+        }
+        /// <summary>
+        /// 是否为空队列
+        /// </summary>
+        /// <returns>true/false</returns>
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+
+        /// <summary>
+        /// 队列中节点个数
+        /// </summary>
+        public int Size
+        {
+            get
+            {
+                return this.size;
+            }
         }
     }
 }
--- Stack_And_Queue/Stack/Program.cs
+++ Stack_And_Queue/Stack/Program.cs
@@ -10,63 +10,170 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("创建一个基于数组的栈：");
+            MyArrayStack<int> myArray = new MyArrayStack<int>(5);
+            Console.WriteLine("入栈0-4：");
+            for (int i = 0; i < 5; i++)
+            {
+                myArray.Push(i);
+            }
+            Console.WriteLine("出栈1次：");
+            Console.WriteLine(myArray.Pop());
+            Console.WriteLine("入栈10：");
+            myArray.Push(10);
+            Console.WriteLine("所有元素出栈：");
+            for (int i = 0,j= myArray.Size; i < j; i++)
+                Console.Write("{0} ",myArray.Pop());
 
+            Console.WriteLine("\r\n---------------------------");
+
+            Console.WriteLine("创建一个基于链表的栈：");
+            MyLinkStack<int> myLinkStack = new MyLinkStack<int>();
+            Console.WriteLine("入栈0-4：");
+            for (int i = 0; i < 5; i++)
+            {
+                myLinkStack.Push(i);
+            }
+            Console.WriteLine("出栈1次：");
+            Console.WriteLine(myLinkStack.Pop());
+            Console.WriteLine("入栈10：");
+            myLinkStack.Push(10);
+            Console.WriteLine("所有元素出栈：");
+            for (int i = 0, j = myLinkStack.Size; i < j; i++)
+                Console.Write("{0} ", myLinkStack.Pop());
+            Console.ReadKey();
         }
     }
     //基于数组的栈
     public class MyArrayStack<T>
     {
-        private T[] nodes;
-        private int index;
+        private T[] nodes;//数据元素
+        private int index;//索引，索引在每次入栈出栈后，会指向最后一个元素的后一个位置
 
         public MyArrayStack(int capacity)
         {
             nodes = new T[capacity];
             index = 0;
         }
+        /// <summary>
+        /// 入栈
+        /// </summary>
+        /// <param name="node"></param>
         public void Push(T node)
         {
-            if (index==nodes.Length)
+            if (index == nodes.Length)//栈满
             {
-
+                ResizeCapacity(index * 2);//改变栈的大小
             }
             nodes[index] = node;
             index++;
         }
+        /// <summary>
+        /// 出栈
+        /// </summary>
+        /// <returns></returns>
         public T Pop()
         {
-            if (index == 0)
+            if (index == 0)//索引为0时是空栈
                 return default(T);
-            T node = nodes[index - 1];
+            T node = nodes[index - 1];//获取最后一个元素
             index--;
             nodes[index] = default(T);
-            if (index > 0 && index == nodes.Length / 4)
+            if (index > 0 && index == nodes.Length / 4)//判断栈的大小
             {
-
+                ResizeCapacity(nodes.Length / 2);//改变大小
             }
             return node;
         }
+        /// <summary>
+        /// 改变栈的大小
+        /// </summary>
+        /// <param name="newCapacity">栈的大小</param>
         private void ResizeCapacity(int newCapacity)
         {
-            T[] newnodes = new T[newCapacity];
-            if (newCapacity > nodes.Length)      
-                newCapacity = nodes.Length;      
-            
-            for (int i = 0; i < newCapacity; i++)   
-                    newnodes[i] = nodes[i];
+            T[] newnodes = new T[newCapacity];//创建新栈
+            if (newCapacity > nodes.Length)    //若新栈的大小比原栈大  
+                newCapacity = nodes.Length;      //当新栈比原栈大时，将所有原栈的值转移到新栈；
+            //当新栈比原栈小时，按照新栈的大小将原栈的值迁移过来，防止溢出
+
+            for (int i = 0; i < newCapacity; i++)
+                newnodes[i] = nodes[i];
 
             nodes = newnodes;
         }
+        /// <summary>
+        /// 是否为空
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
-            return this.index == 0;
+            return index == 0;
         }
+        /// <summary>
+        /// 获取栈的大小
+        /// </summary>
         public int Size
         {
             get
             {
-                return this.index;
+                return index;
             }
+        }
+    }
+    //节点定义
+    class Node<T>
+    {
+        public T item { get; set; }
+        public Node<T> Next { get; set; }
+        public Node() { }
+        public Node(T item)
+        {
+            this.item = item;
+        }
+    }
+    //基于链表的栈
+    class MyLinkStack<T>{
+        private Node<T> first;
+        private int index;
+        public MyLinkStack()
+        {
+            first = null;
+            index = 0;
+        }
+        /// <summary>
+        /// 入栈
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(T value)
+        {
+
+            Node<T> newnode = new Node<T>(value);//生成新节点
+            newnode.Next = first;//新节点的后继指向表头
+            first = newnode;//新节点成为新的表头
+
+            //Node<T> oldNode = first;
+            //first = new Node<T>(value);
+            //first.Next = oldNode;
+            index++;
+        }
+        /// <summary>
+        /// 出栈
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            T value = first.item;//获取要弹出的值
+            first = first.Next;//表头指向下一个元素
+            index--;
+            return value;
+        }
+        public bool IsEmpty()
+        {
+            return index == 0;
+        }
+        public int Size
+        {
+            get { return index; }
         }
     }
 }
